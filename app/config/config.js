0d4991ea5bd651aa34b77b329b61fb9f2ebfa775
Nazmul Hasan <nhmnazmul87@gmail.com>
2024-09-09 13:08:57 +0600

    Statar File Completed for Rest Api Development

--- app/config/config.js
+++ app/config/config.js
@@ -4,3 +4,21 @@
  * Late Update: no  Update here
  * ============= Application Configuration =============
  */
+
+export const PORT = 5000;
+export const DATABASE_URI = "";
+export const JWT_KEY = "";
+export const JWT_EXPIRED_TIME = 30 * 24 * 60 * 60;
+
+export const EMAIL_HOST = "";
+export const EMAIL_PORT = "";
+export const EMAIL_USER = "";
+export const EMAIL_PASS = "";
+
+export const WEB_CACHE = false;
+export const MAX_JSON_FILE = "10MB";
+export const URL_ENCODED = true;
+
+export const REQUEST_TIME = 20 * 60 * 1000;
+export const REQUEST_LIMIT = 2000;
+
